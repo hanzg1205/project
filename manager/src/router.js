886abf228655bd65cbf4d920167d9b016340330c
hanzg1205 <1527842986@qq.com>
2019-06-10 21:05:28 +0800

    修改router.js

--- manager/src/router.js
+++ manager/src/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import Login from './views/login/index';
 import Meun from './views/Meun/Meun';
+// import Home from './views/home';
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
