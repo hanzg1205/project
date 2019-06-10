bc9426d6b494e47b26ac5f8715c3ad17f9ad1d72
hanzg1205 <1527842986@qq.com>
2019-06-10 19:19:35 +0800

    登录页跳转

--- manager/src/router.js
+++ manager/src/router.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import Login from './views/login/index';
-
+import Meun from './views/Meun/Meun';
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
+        <Route path="/meun" exact component={Meun} />
         <Route path="/" exact component={Login} />
       </Switch>
     </Router>
--- manager/src/views/login/index.js
+++ manager/src/views/login/index.js
@@ -62,8 +62,7 @@ class Login extends Component {
         this.props.form.validateFields((err, values) => {
           if (!err) {
             console.log('Received values of form: ', values);
-          }else{
-            // this.props.history.push('/')
+            this.props.history.push('/meun')
           }
         });
       };
