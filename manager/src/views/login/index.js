702e1fc99cb88e1501ed46353fcea707931b568a
sunliang0928 <sunliang0928@163.com>
2019-06-11 21:33:26 +0800

    登陆页测试

--- manager/src/views/login/index.js
+++ manager/src/views/login/index.js
@@ -1,71 +1,86 @@
-import React, { Component } from 'react';
+import React, { useState , useEffect } from 'react';
 import style from './login.css';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import 'antd/dist/antd.css';
-
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <div className={style.login_wrapper}>
-                <div className={style.login_form}>
-                    <Form onSubmit={this.handleSubmit} className="login-form">
-                        <Form.Item>
-                            {getFieldDecorator('username', {
-                                rules: [{ required: true, message: '请输入您的用户名!' }],
-                            })(
-                                <Input
-                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                    placeholder="请输入用户名"
-                                    className={style.login_input}
-                                />,
-                            )}
-                        </Form.Item>
-                        <Form.Item>
-                            {getFieldDecorator('password', {
-                                rules: [{ required: true, message: '请输入您的密码!' }],
-                            })(
-                                <Input
-                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                    type="password"
-                                    placeholder="请输入密码"
-                                    className={style.login_input}
-                                />,
-                            )}
-                        </Form.Item>
-                        <Form.Item>
-                            <div className={style.login_form_remember}>
-                                {getFieldDecorator('remember', {
-                                    valuePropName: 'checked',
-                                    initialValue: true,
-                                })(<Checkbox>记住密码</Checkbox>)}
-                                <a className="login-form-forgot" href="">
-                                    忘记密码
-                                </a>
-                            </div>
-                            
-                            <Button type="primary" htmlType="submit" className={style.login_form_button}>
-                                登 录
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </div>
-            </div>
-        );
-    }
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-          if (!err) {
-            console.log('Received values of form: ', values);
-            this.props.history.push('/meun')
-          }
+import { connect } from 'dva';
+function Login(props){
+    const { getFieldDecorator } = props.form;
+    let  { login } = props
+    useEffect(()=>{
+            login({
+                user_name:'chenmanjie',
+                user_pwd:'Chenmanjie123!'
+            })
+        },[])
+        console.log(props)
+        let state=useState(props.user)
+        let handleSubmit = e => {
+            e.preventDefault();
+            this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                this.props.history.push('/meun')
+            }
         });
       };
+     return (
+        <div className={style.login_wrapper}>
+            <div className={style.login_form}>
+                <Form onSubmit={handleSubmit} className="login-form">
+                    <Form.Item>
+                        {getFieldDecorator('username', {
+                            rules: [{ required: true, message: '请输入您的用户名!' }],
+                            validateTrigger:'onInput'
+                        })(
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="请输入用户名"
+                                className={style.login_input}
+                            />,
+                        )}
+                    </Form.Item>
+                     <Form.Item>
+                        {getFieldDecorator('password', {
+                            rules: [{ required: true, message: '请输入您的密码!' },{pattern: /^(?:(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])).*$/
+                                , message: '请输入正确密码!' }]
+                            })(
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="请输入密码"
+                                className={style.login_input}
+                            />,
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                        <div className={style.login_form_remember}>
+                            {getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true,
+                            })(<Checkbox>记住密码</Checkbox>)}
+                            <a className="login-form-forgot" href="">
+                                忘记密码
+                            </a>
+                        </div>
+                        <Button type="primary" htmlType="submit" className={style.login_form_button}>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    )
 }
-
-export default Form.create()(Login);
+const mapStateToProps=(state)=>{
+    return {
+        ...state
+    }
+}
+const mapDispatchToProps=(dispatch)=>{
+    return {
+       login(payload){
+        dispatch({type:'user/login',payload})
+       }
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Form.create()(Login))
